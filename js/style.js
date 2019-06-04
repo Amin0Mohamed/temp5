618784958fa0c36d7fc899037a418a013d5e1bda
amin mohamed <aminelcaptain20@gmail.com>
2019-06-04 16:40:24 +0200

    add all file in git

---
+++ js/style.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+$(window).on("scroll",function(){
+	
+	if( $("window").scrollTop() >= 500 ){
+		  
+		 $(" #home .cover .nav").css('background-color','#000');
+		 }
+	  
+	});
